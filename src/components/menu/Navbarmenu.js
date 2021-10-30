63e4a7bfae0e66796cb58290dd9906026d064b91
Hector <hechernandez30@gmail.com>
2021-10-30 14:35:05 -0600

    Prueba 25

--- src/components/menu/Navbarmenu.js
+++ src/components/menu/Navbarmenu.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import {NavLink, Link} from 'react-router-dom';
-import {FiAlignRight,FiXCircle,FiChevronDown } from "react-icons/fi";
+
 import logo from '../../img/logo.png';
 
 
