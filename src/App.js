15d6d3380be50a5e524d086ff939e029997b0f13
Hector <hechernandez30@gmail.com>
2021-10-30 03:54:02 -0600

    Prueba 9

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-      fffffffffffffffffffffffffffffffffffffffffffffffff
+      
     );
   }
 }
