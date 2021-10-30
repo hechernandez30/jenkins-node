dea62d1a0d00f5154732852aa537fbfe70368107
Hector <hechernandez30@gmail.com>
2021-10-30 13:55:21 -0600

    Prueba 21

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+   import ReactDOM from "react-dom";
+   import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+   import Page1 from "./Page1";
+   import Page2 from "./Page2";
+
+    const rootElement = document.getElementById("root");
+    ReactDOM.render(
+      <BrowserRouter>
+       <Switch>
+        <Route exact path="/" component={Page1} />
+        <Route path="/page2" component={Page2} />
+      </Switch>
+      </BrowserRouter>,
+      rootElement
+    );
---
+++ src/page2.js
@@ -0,0 +1,13 @@
+  import React from "react";
+
+   function Page2() {
+
+        return (
+          <div>
+            <p>sdfgsdg</p>
+            </div>          
+        );
+
+    }
+
+    export default Page2;
