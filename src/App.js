01d6d89f99fcd2c840c882b210137e23333b8be6
hechernandez30 <90530372+hechernandez30@users.noreply.github.com>
2021-10-30 02:19:53 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,28 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <!--<div className="App">
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Docker-Meetup</h1>
         </header>
         <p className="App-intro">
-          Welcome to this Docker Meetup with Jenkins*********************
+          Welcome to this Docker Meetup with Jenkins
 
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>-->
-      <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login Here | Fazt</title>
-    <link rel="stylesheet" href="css/master.css">
-  </head>
-  <body>
-Este es una prueba, veremos como sucede todo...
-  </body>
-</html>
+      </div>
     );
   }
 }
