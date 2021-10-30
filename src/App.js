53c2624f8ca0db98bc620e9f8255776f2981701e
Hector <hechernandez30@gmail.com>
2021-10-30 13:24:37 -0600

    Prueba 18

--- src/App.js
+++ src/App.js
@@ -8,16 +8,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">			
-      <nav class="menu">
+        <header className="App-header">
+         
+         
+         
+         <nav class="menu">
 		<ul>
 			<li><a href="/index.html"> Inicio</a></li>
 			<li><a href="productos.html">Productos</a></li>
 			<li><a href="contacto.html">Contacto</a></li>
 		</ul>
 	</nav>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenido Hector to Docker-Meetup</h1>
+         
+         
+         
           
         </header>
         <div clas="secundaarios">
