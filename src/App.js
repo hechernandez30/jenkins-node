efc6311f7a29b3bb38d3c84cb67c648472bbc305
Hector <hechernandez30@gmail.com>
2021-10-30 13:46:20 -0600

    Prueba 20

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">			
-        <header className="App-header">
+        
          
          
          
@@ -23,15 +23,9 @@ class App extends Component {
          
          
           
-        </header>
-        <div clas="secundaarios">
         
-        </div>
-        <p className="App-intro">
-          Welcome to this Docker Meetup with Jenkins
-
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        
       </div>
       
       
