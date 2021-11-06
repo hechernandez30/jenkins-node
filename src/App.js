2c7bd681914a1401d57641df8fb13db81778e0ff
Hector Hernandez <hechernandez30@gmail.com>
2021-11-05 23:29:41 -0600

    Prueba 55

--- src/App.js
+++ src/App.js
@@ -4,26 +4,43 @@ import Category from './Category.js';
 import Inicio from './Inicio';
 import Productos from './Productos';
 import Contacto from './Contacto';
-import { BrowserRouter, Router, Switch } from
-"react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ul>
-        <Switch>
-        <Route path='/' exact component={Inicio} />
-        <Route path='/productos' exact component={Productos} />
-        <Route path='/contacto' exact component={Contacto} />
           <Category name="Pagina de Inicio" items={['Mision','Vision','Objetivos']}/>
           <Category name="Productos" items={['Ofertas del Mes','Black Friday','En liquidacion']}/>
           <Category name="Contacto" items={['Sedes','Ubicacion','Video Informativo']}/>
-          </Switch>
         </ul>
       </div>
     );
   }
 }
+
+
+
+var Component = React.createClass({
+  getInitialState: function() { return {query: ''} },
+  queryChange: function(evt) {
+    this.setState({query: evt.target.value});
+  },
+  _buildLinkHref: function() {
+    return '/search/'+this.state.query+'/some-action';
+  },
+  render: function() {
+    return (
+      <div className="component-wrapper">
+        <input type="text" value={this.state.query} />
+        <a href={this._buildLinkHref()} className="button">
+          Search
+        </a>
+      </div>
+    );
+  }
+});
+
+
 
 export default App;
